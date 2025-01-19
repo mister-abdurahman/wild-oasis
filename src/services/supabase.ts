0e2fdb01dff08b3001d1af6dd2012554deb24fd8
aramide ramadan <aramramadan7@gmail.com>
2025-01-19 01:13:32 +0100

    reloaded project to new db

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,7 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://tiqrnepcxnpaztuvztgv.supabase.co";
+export const supabaseUrl = "https://umwjevpyasylkyfwtmkh.supabase.co";
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
+
+// public: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVtd2pldnB5YXN5bGt5Znd0bWtoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzcyNDA2NDQsImV4cCI6MjA1MjgxNjY0NH0.j8WVJ4kBhHEUEsJvtApLFds6YnWHj0W8_Gus4GANW8c
